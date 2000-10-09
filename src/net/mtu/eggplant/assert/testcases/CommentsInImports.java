496cf84b89c8e3b0d6f7bd81f1f2936d4be632b7
Jon Schewe <jpschewe@mtu.net>
2000-10-09 12:57:35 +0000

    *** empty log message ***
    git-svn-id: https://jon/svn/JonsAssert/trunk@90 94f1d44e-47f0-0310-857f-9ad392bb85a4

---
+++ src/net/mtu/eggplant/assert/testcases/CommentsInImports.java
@@ -0,0 +1,22 @@
+/*
+  This file is licensed through the GNU public License.  Please read it.
+  Basically you can modify this code as you wish, but you need to distribute
+  the source code for all applications that use this code.
+
+  I'd appreciate comments/suggestions on the code jpschewe@eggplant.mtu.net
+*/
+package org.tcfreenet.schewe.assert.testcases;
+
+import java.util.Vector;
+/**
+   Intermingled comment.
+**/
+import java.util.List;
+import java.util.Set;
+
+/**
+   Check for comments in between imports.
+**/
+public class CommentsInImports {
+
+}
