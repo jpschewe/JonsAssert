11df2e673b51ef3c45e8d535df094d24c6cfe4f9
Jon Schewe <jpschewe@mtu.net>
2000-12-28 04:59:38 +0000

    Updated for new paths.
    git-svn-id: https://jon/svn/JonsAssert/trunk@109 94f1d44e-47f0-0310-857f-9ad392bb85a4

---
+++ prj.el
@@ -0,0 +1,20 @@
+;;This file assumes that the following variables and methods exist:
+;;assert-root - root of the project
+;;java-vm - jvm to use
+;;java-home - root of java install directory
+(jde-set-project-name "JonsAssert")
+(jde-set-variables
+ '(jde-run-working-directory (expand-file-name "src/" assert-root))
+ '(jde-run-read-app-args t)
+ '(jde-global-classpath (list
+			 (expand-file-name "src/" assert-root)
+			 (expand-file-name "src/" infra-root)
+			 (expand-file-name "lib/antlr.jar" assert-root)
+			 (expand-file-name "lib/junit.jar" assert-root)
+			 (expand-file-name "lib/java-getopt-1.0.7.jar" assert-root)
+			 (expand-file-name "jre/lib/rt.jar" java-home)
+			 ))
+ '(jde-compile-option-deprecation t)
+ '(jde-run-java-vm java-vm)
+ '(jde-run-option-vm-args '("-DASSERT_BEHAVIOR=CONTINUE "))
+ )
